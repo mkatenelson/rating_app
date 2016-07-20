530d0c8c4a8dc02fb4999998a3540ba1f5db645b
Kate Nelson <mkatenelson@gmail.com>
2016-07-19 17:11:48 -0700

    added logout

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719031844) do
+ActiveRecord::Schema.define(version: 20160719004034) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 20160719031844) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "user_id"
-    t.integer  "place_id"
   end
 
   create_table "users", force: :cascade do |t|
