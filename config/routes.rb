34ba4b61c6a06bbbd58311f7d62f51e0dac77d29
Kate Nelson <mkatenelson@gmail.com>
2016-07-20 16:45:03 -0700

    google map displaying on homepage through api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   devise_for :users
+  resources :users, only: [:show] do
+    resources :reviews, only: [:show, :edit]
+  end
+
 
   resources :places do
     collection do
@@ -9,7 +13,7 @@ Rails.application.routes.draw do
     resources :reviews, except: [:show, :index]
   end
 
-  root 'places#index'
+  root 'home#index'
 
 
 end
