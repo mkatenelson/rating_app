fc7710299298c725b9330f2c4577802956bf0520
Kate Nelson <mkatenelson@gmail.com>
2016-07-21 16:30:06 -0700

    post-presentation

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,7 +6,7 @@ class Place < ActiveRecord::Base
 
   has_attached_file :image,
     styles: {
-      medium: "400x300#", thumb: "100x100>"
+      small: "400x300#", thumb: "100x100>"
     },
     default_url: "http://telclarity.com/wp/wp-content/themes/pantherhead/assets/images/carousel-no-image-cropped-400x300.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
